289138f8bbc2067d861dc0530725f5f5f193b9d3
Anjjellan <sam_santhakumar-a@ulster.ac.uk>
2026-09-24 18:26:35 +0300

    Task 1

--- Week-1-Practical/Program.cs
+++ Week-1-Practical/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hello {name}, you will be {age + 5} in 5 years.");
